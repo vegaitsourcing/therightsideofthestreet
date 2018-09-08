a747ac3a6a2ea234c8672d053dba5c1e35253e8b
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2018-09-08 22:14:34 +0200

    Finished DonationsAndMembership.cshtml

---
+++ PravaStranaUlice.Models/Generated/DonationsAndMembership.cs
@@ -0,0 +1,13 @@
+﻿using PravaStranaUlice.Models.Extensions;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace PravaStranaUlice.Models
+{
+	public partial class DonationsAndMembership : PublishedContentModel, IPage
+	{
+		public string EmbedVideo 
+		{
+			get { return this.GetPropertyValue<string>("video").Replace("watch?v=","embed/"); }
+		}
+	}
+}
