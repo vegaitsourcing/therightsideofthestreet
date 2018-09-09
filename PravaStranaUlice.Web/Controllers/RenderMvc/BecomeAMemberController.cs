95733a67632d166fe1703b24e4ff50574cabb8fd
Dusan Kojcic <dusan.kojcic@praktikant-001.vega.local>
2018-09-09 11:59:41 +0200

    BecomeAMember cshtml povezan

---
+++ PravaStranaUlice.Web/Controllers/RenderMvc/BecomeAMemberController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PravaStranaUlice.Models;
+using Umbraco.Web.Mvc;
+
+namespace PravaStranaUlice.Web.Controllers.RenderMvc
+{
+    public class BecomeAMemberController : RenderMvcController
+    {
+        public ActionResult Index(BecomeAmember model)
+        {
+            return CurrentTemplate(model);
+        }
+    }
+}
