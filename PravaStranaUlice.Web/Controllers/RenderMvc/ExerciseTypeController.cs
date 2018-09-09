696d848adf8d11de2d3d69839e76c8616d4f3a05
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2018-09-09 02:43:51 +0200

    Created ExerciseTypeController.cs; Started ExerciseType.cshtml

---
+++ PravaStranaUlice.Web/Controllers/RenderMvc/ExerciseTypeController.cs
@@ -0,0 +1,14 @@
+﻿using PravaStranaUlice.Models;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace PravaStranaUlice.Web.Controllers.RenderMvc
+{
+	public class ExerciseTypeController : RenderMvcController
+	{
+		public ActionResult Index(ExerciseType model)
+		{
+			return CurrentTemplate(model);
+		}
+	}
+}
