d7128cddac6de43013c18162f6e6ab5a9e24c2df
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2018-09-09 12:45:52 +0200

    Updated ExerciseType.cshtml

---
+++ PravaStranaUlice.Models/Extensions/StringExtensions.cs
@@ -0,0 +1,18 @@
+﻿using PravaStranaUlice.Common.Extensions;
+using System.Web.Mvc;
+
+namespace PravaStranaUlice.Models.Extensions
+{
+	public static class StringExtensions
+	{
+		/// <summary>
+		/// Returns name of the controller class after stripping "controller" suffix from it.
+		/// </summary>
+		/// <param name="controllerName">The name of the controller class.</param>
+		/// <returns>Name without "controller" suffix.</returns>
+		public static string RemoveControllerSuffix(this string controllerName)
+		{
+			return controllerName.RemoveSuffix(nameof(Controller));
+		}
+	}
+}
