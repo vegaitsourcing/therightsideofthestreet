b60dcf352f0a66572f7153db43bcded7e567aaf6
Katarina Ivanic <k.ivanic@vegaitsourcing.rs>
2018-09-09 18:23:44 +0200

    Created ExerciseLandingPage.cshtml

---
+++ PravaStranaUlice.Web/Controllers/RenderMvc/ExerciseLandingPageController.cs
@@ -0,0 +1,14 @@
+﻿using PravaStranaUlice.Models;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace PravaStranaUlice.Web.Controllers.RenderMvc
+{
+	public class ExerciseLandingPageController : RenderMvcController
+	{
+		public ActionResult Index(ExerciseLandingPage model)
+		{
+			return CurrentTemplate(model);
+		}
+	}
+}
