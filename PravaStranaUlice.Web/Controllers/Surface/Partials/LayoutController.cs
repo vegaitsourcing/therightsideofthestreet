26b6bdf8452d800b855f828ac5772a344fcf40ea
Zlatko Spasojevic <z.spasojevic@vegaitsourcing.rs>
2018-10-10 17:17:32 +0200

    Layout implementation

---
+++ PravaStranaUlice.Web/Controllers/Surface/Partials/LayoutController.cs
@@ -0,0 +1,18 @@
+﻿using PravaStranaUlice.Web.ViewModels.Partials.Layout;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PravaStranaUlice.Web.Controllers.Surface.Partials
+{
+	public class LayoutController : BasePartialController
+	{
+		[ChildActionOnly]
+		public ActionResult MetaTags(MetaTagsViewModel viewModel)
+		{
+			return PartialView(viewModel);
+		}
+	}
+}
