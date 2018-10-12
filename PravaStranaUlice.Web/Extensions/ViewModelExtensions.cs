61a4f8279199279aca007aefad6095df4c873cf2
Jovan Karanovic <j.karanovic@vegaitsourcing.rs>
2018-10-12 16:12:13 +0200

    Added footer page with all functionality

--- PravaStranaUlice.Web/Extensions/ViewModelExtensions.cs
+++ PravaStranaUlice.Web/Extensions/ViewModelExtensions.cs
@@ -74,15 +74,6 @@ namespace PravaStranaUlice.Web.Extensions
 
 		//public static XMLSitemapItemViewModel AsXMLSitemapItemViewModel(this IPage page)
 		//	=> page != null ? new XMLSitemapItemViewModel(page) : null;
-
-		public static List<List<T>> Split<T>(this List<T> items, int sliceSize = 4)
-		{
-			List<List<T>> list = new List<List<T>>();
-			for (int i = 0; i < items.Count; i += sliceSize)
-
-				list.Add(items.GetRange(i, Math.Min(sliceSize, items.Count - i)));
-
-			return list;
-		}
+        
 	}
 }
--- PravaStranaUlice.Web/ViewModels/Partials/Layout/FooterViewModel.cs
+++ PravaStranaUlice.Web/ViewModels/Partials/Layout/FooterViewModel.cs
@@ -1,14 +1,13 @@
-﻿using PravaStranaUlice.Models.DocumentTypes;
+﻿using PravaStranaUlice.Common.Extensions;
+using PravaStranaUlice.Models.DocumentTypes;
 using PravaStranaUlice.Web.Contexts;
+using PravaStranaUlice.Web.Extensions;
+using PravaStranaUlice.Web.ViewModels.Partials.NestedContent;
 using PravaStranaUlice.Web.ViewModels.Shared;
+using RJP.MultiUrlPicker.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using PravaStranaUlice.Web.Extensions;
-using PravaStranaUlice.Models.Extensions;
-using PravaStranaUlice.Common.Extensions;
-using PravaStranaUlice.Web.ViewModels.Partials.NestedContent;
 
 namespace PravaStranaUlice.Web.ViewModels.Partials.Layout
 {
@@ -21,16 +20,27 @@ namespace PravaStranaUlice.Web.ViewModels.Partials.Layout
             InfoImage = context.Home.InfoImage.AsViewModel();
 			InfoTitle = context.Home.InfoTitle;
 			InfoText = context.Home.InfoText;
-			HygieneLinksGroup = context.Home.GetNavigationItems<IPage>().AsNavigationViewModel().ToList();
-			ContactEmail = context.Home.ContactEmail;
+            HygieneLinksGroup = GetHygieneLinksGroups(context.Home.HygieneLinks);
+            ContactEmail = context.Home.ContactEmail;
             SocialLinks = context.Home.SocialLinks?.Select(sl => context.WithNestedContent(sl).AsViewModel<SocialLinkViewModel>()).AsList();
         }
 
         public ImageViewModel InfoImage { get; }
 		public string InfoTitle { get; }
 		public string InfoText { get; }
-		public List<PrimaryNavigationItemViewModel> HygieneLinksGroup { get; } 
+        public IList<IEnumerable<LinkViewModel>> HygieneLinksGroup { get; }
 		public string ContactEmail { get; }
         public IList<SocialLinkViewModel> SocialLinks { get; }
+
+        private IList<IEnumerable<LinkViewModel>> GetHygieneLinksGroups(IEnumerable<Link> links)
+        {
+            if (links == null) return new List<IEnumerable<LinkViewModel>>();
+
+            IList<LinkViewModel> linksList = links.Select(lnk => lnk.AsViewModel()).AsList();
+            const int numberOfGroups = 2;
+
+            return linksList.Split((int)Math.Ceiling((double)linksList.Count / numberOfGroups)).ToList();
+        }
+
 	}
 }
