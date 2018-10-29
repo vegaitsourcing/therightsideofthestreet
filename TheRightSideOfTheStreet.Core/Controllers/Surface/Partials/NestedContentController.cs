67f7b115c485430a70e3b826cd75faeb1f07cead
Zlatko Spasojevic <z.spasojevic@vegaitsourcing.rs>
2018-10-29 10:37:35 +0100

    Removed unnecessary using

--- TheRightSideOfTheStreet.Core/Controllers/Surface/Partials/NestedContentController.cs
+++ TheRightSideOfTheStreet.Core/Controllers/Surface/Partials/NestedContentController.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using TheRightSideOfTheStreet.Core.Extensions;
 using TheRightSideOfTheStreet.Core.ViewModels.Partials.NestedContent;
-using TheRightSideOfTheStreet.Core.ViewModels.Partials.NestedContent.Modules;
 
 namespace TheRightSideOfTheStreet.Core.Controllers.Surface.Partials
 {
