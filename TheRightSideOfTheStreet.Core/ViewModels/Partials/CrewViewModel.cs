93e5b35683a189a1b3665614ce09e88f09167f92
Zlatko Spasojevic <z.spasojevic@vegaitsourcing.rs>
2018-12-04 09:36:54 +0100

    Removed unnecessary using

--- TheRightSideOfTheStreet.Core/ViewModels/Partials/CrewViewModel.cs
+++ TheRightSideOfTheStreet.Core/ViewModels/Partials/CrewViewModel.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web;
 using TheRightSideOfTheStreet.Core.Extensions;
 using TheRightSideOfTheStreet.Core.ViewModels.Shared;
