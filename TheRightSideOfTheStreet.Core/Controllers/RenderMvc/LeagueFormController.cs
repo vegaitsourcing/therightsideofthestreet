475d6fff40aa277c80918e67b3a565fd156b48a8
Darko Opala <d.opala@vegaitsourcing.rs>
2018-12-11 11:31:43 +0100

    Added controllers for the League Form and SW League.

---
+++ TheRightSideOfTheStreet.Core/Controllers/RenderMvc/LeagueFormController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using TheRightSideOfTheStreet.Core.Extensions;
+using TheRightSideOfTheStreet.Core.ViewModels;
+using TheRightSideOfTheStreet.Models;
+
+namespace TheRightSideOfTheStreet.Core.Controllers.RenderMvc
+{
+	public class LeagueFormController : BasePageController<LeagueForm>
+	{
+		public ActionResult Index(LeagueForm model) => CurrentTemplate(new LeagueFormViewModel(Umbraco.CreatePageContext(model)));
+	}
+}
---
+++ TheRightSideOfTheStreet.Core/Controllers/RenderMvc/SwLeagueController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+using TheRightSideOfTheStreet.Core.Extensions;
+using TheRightSideOfTheStreet.Core.ViewModels;
+using TheRightSideOfTheStreet.Models;
+
+namespace TheRightSideOfTheStreet.Core.Controllers.RenderMvc
+{
+	public class SwLeagueController : BasePageController<SWleague>
+	{
+		public ActionResult Index(SWleague model) => CurrentTemplate(new SwLeagueViewModel(Umbraco.CreatePageContext(model)));
+	}
+}
