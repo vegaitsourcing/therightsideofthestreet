eb82affa69177a7ef82d4d78d0d3112b22914000
Zlatko Spasojevic <z.spasojevic@vegaitsourcing.rs>
2019-03-11 12:17:38 +0100

    Updated AthleteModuleViewModel

--- TheRightSideOfTheStreet.Core/ViewModels/Partials/NestedContent/Modules/AthleteModuleViewModel.cs
+++ TheRightSideOfTheStreet.Core/ViewModels/Partials/NestedContent/Modules/AthleteModuleViewModel.cs
@@ -5,6 +5,7 @@ using TheRightSideOfTheStreet.Core.Contexts;
 using TheRightSideOfTheStreet.Core.Extensions;
 using TheRightSideOfTheStreet.Core.Search;
 using TheRightSideOfTheStreet.Models;
+using Umbraco.Web;
 
 namespace TheRightSideOfTheStreet.Core.ViewModels.Partials.NestedContent.Modules
 {
@@ -14,10 +15,12 @@ namespace TheRightSideOfTheStreet.Core.ViewModels.Partials.NestedContent.Modules
 		{
 			Title = context.NestedContent.Title;
 			Athletes = new AthleteMembersSearch().GetAthletes().Select(am => new AthleteMemberPreviewViewModel(context.WithAthleteMember(am))).Where( m => m.PreviewImage != null).AsList();
+			Admins = new AthleteMembersSearch().GetAdmin().Select(adm => new AdminPreviewViewModel(context.WithAthleteMember(adm))).Where(m => m.PreviewImage != null).AsList();
 		}
 
 		public string Title { get; }
 		public IList<AthleteMemberPreviewViewModel> Athletes { get; }
+		public IList<AdminPreviewViewModel> Admins { get; }
 		
 	}
 }
